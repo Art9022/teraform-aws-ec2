1951c7056c344ff87d31d917aa5966a36f36cb2c
Artur Ayvazyan <118291017+Art9022@users.noreply.github.com>
2023-01-21 10:51:12 +0400

    Add

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
 terraform {
-  cloud {
-    organization = "artur_ayvazyan"
-
-    workspaces {
-      name = "homework_2"
-    }
-  }
-}
-
-terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -24,63 +14,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami                    = "ami-0b5eea76982371e91"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.My_webServer.id ]
-  user_data = file("user_data.sh")
- tags = {
-   "name" = var.name
-}
-}
+  
 
 resource "aws_vpc" "frst_vpc" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-   "name" = var.name
-}
 }
 
 resource "aws_subnet" "frst_subnet" {
   vpc_id     = aws_vpc.frst_vpc.id
   cidr_block = "10.0.1.0/24"
-  tags = {
-   "name" = var.name
-}
 }
 
 resource "aws_network_interface" "interface_0" {
-  subnet_id       = aws_subnet.frst_subnet.id
-  private_ips     = ["10.0.0.50"]
-  security_groups = [ "aws_security_group.My_webServer" ]
+  subnet_id = aws_subnet.frst_subnet.id
+  private_ips = ["10.0.1.10"]
 
-  attachment {
-    instance     = aws_instance.app_server.id
-    device_index = 1
-  }
-tags = {
-   "name" = var.name
-}
 }
 
-
-
-resource "aws_security_group" "My_webServer" {
+resource "aws_security_group" "My_sg" {
   name        = var.name
-  description = "My First secgrup"
-    tags = {
-    name  = var.name
-   
-
-  }
+  description = "SG for ec2"
+  vpc_id      = aws_vpc.frst_vpc.id
 
   ingress {
-    
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
   }
 
   egress {
@@ -90,7 +50,19 @@ resource "aws_security_group" "My_webServer" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
 }
-  
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  user_data = file("user_data.sh")
+  security_groups = [aws_security_group.My_sg.name]
+  network_interface {
+    network_interface_id = aws_network_interface.interface_0.id
+    device_index = 1
+  }
+  
+  tags = {
+    "name" = var.name
+  }
+}
