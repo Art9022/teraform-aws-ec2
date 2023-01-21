3aafd88119d1d45400937a94b0cd30566add2959
Artur Ayvazyan <118291017+Art9022@users.noreply.github.com>
2023-01-21 12:18:49 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,18 +8,5 @@ output "instance_public_ip" {
   value       = aws_instance.app_server.public_ip
 }
 
-output "My_webServer_sg_id" {
-  description = "SG id of the EC2 instance"
-  value       = aws_security_group.My_webServer.id
-}
 
-output "My_webServer_sg_arn" {
-  description = "ARN SG of the EC2 instance"
-  value       = aws_security_group.My_webServer.arn
-}
-
-output "My_webServer_sg_ingress" {
-  description = "Ingress SG of the EC2 instance"
-  value       = aws_security_group.My_webServer.ingress
-}
 
